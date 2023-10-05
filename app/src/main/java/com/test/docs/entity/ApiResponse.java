c8fbe13c09d656d5e3e54787bc76632f368e45f2
nithinTest10 <146797806+nithinTest10@users.noreply.github.com>
2023-10-05 10:50:51 -0400

    Update ApiResponse.java --getters and setters

--- app/src/main/java/com/test/docs/entity/ApiResponse.java
+++ app/src/main/java/com/test/docs/entity/ApiResponse.java
@@ -10,4 +10,22 @@ public class ApiResponse<T> {
     private int status;
     private String message;
     private String sessionId;
+    public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getSessionId() {
+		return sessionId;
+	}
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
 }
